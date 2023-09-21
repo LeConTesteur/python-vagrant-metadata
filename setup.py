6a4ce27c437beffe6e38667f1d02de052a0636e7
Le ConTesteur <>
2023-09-21 19:21:33 +0200

    Release v0.0.4

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ extras = {
 
 setuptools.setup(
     name="python-vagrant-metadata",
-    version="0.0.3",
+    version="0.0.4",
     author="LeConTesteur",
     description="Can use and parse metadata information of vagrant box",
     long_description=long_description,
